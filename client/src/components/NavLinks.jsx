6364c5f6b09f5047f8cc9f783defbfc86d62d820
Sam Getlan <samgetlan@gmail.com>
2018-04-16 18:41:02 -0700

    Set up barebones react structure to test environment setup

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const NavLinks = props => <p>test</p>;
+
+export default NavLinks;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,20 @@
 import React, { Component } from 'react';
+import NavLinks from './Navlinks.jsx';
+import InterestedLinks from './InterestedLinks.jsx';
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
 
 
+  render() {
+    return (
+      <div className="navbarContainer">
+        <NavLinks />
+        <InterestedLinks />
+      </div>
+    );
+  }
+}
 export default Navbar;
