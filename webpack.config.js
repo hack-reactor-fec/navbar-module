b520d9d371b6b588879751e94756b71db66bdc6e
Sam Getlan <samgetlan@gmail.com>
2018-04-16 17:45:16 -0700

    Began to set up dev environment

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+  },
+  devtool: '#eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+  watch: true,
+};
